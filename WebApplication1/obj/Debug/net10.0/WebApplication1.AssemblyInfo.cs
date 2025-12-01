59b86a0f14a1ef383eaf661ff104352bf57ce890
PFS-Horie0107 <hhorie@proface-sys.com>
2025-12-01 14:09:49 +0900

    「Test001.js」追加 Fixes #27

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+979a32100160d268dc453f92fec8be7af63609d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693bcbea933b37c0fc18086ada14bf9f73fd0d20")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
